62f672804781ecc11d78c4ee1bbd7e3f76b3d612
prmayanksingh <prmayanksingh05@gmail.com>
2024-09-02 19:45:36 +0530

    settimeout

--- javascript/js_basics/Asynchronous.js
+++ javascript/js_basics/Asynchronous.js
@@ -5,8 +5,21 @@
 
 
 //ye sabhi use hi tab kiye jaate hai jab aapko kuch aisa karna ho jismein time lagega
-// settimeout
-// setinterval
-// Fetch API
-// Axios (or other HTTP libraries)
-// promise
+
+// settimeout - settimeout ka code kuchh der  baad chalta hai.
+// console.log("hey 1"); // synchronous
+// console.log("hey 2"); // synchronous
+// setTimeout(function{
+//     console.log("hey 3"); // asynchronous
+// },2000)
+// console.log("hey 4"); // synchronous
+
+
+// setinterval - set interval ka code kuchh der baad chalta hai baar baar ek particular interval time mein.
+
+
+// Fetch API - ye kisi url (API) par jaa kar kuchh data laayega ya data humaare paas se us url (API) par lekar jaayega.
+
+// Axios (or other HTTP libraries) - ye bhi wahi karega jo fetch karta hai bas ye thoda jaada developer friendly hai.
+
+// promise - isme jo bhi code likhoge wo apana kaam karega aur ye khud side stack mein chala jaayega us code ko lekar aur jab andar se code ko resolve kiya jaayega tab ye chalega.
