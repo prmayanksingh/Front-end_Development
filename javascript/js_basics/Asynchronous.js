d9b2baa76dd8c2fec67184b78059155e78e973d5
prmayanksingh <prmayanksingh05@gmail.com>
2024-09-02 19:20:34 +0530

    asynchronous js

---
+++ javascript/js_basics/Asynchronous.js
@@ -0,0 +1,12 @@
+// ASYNC JS
+
+
+// aisa koi bhi code jismein kuchh time lagta hai, js mein use by default in most cases async code maan kar side stack mein daal deya jaata hai
+
+
+//ye sabhi use hi tab kiye jaate hai jab aapko kuch aisa karna ho jismein time lagega
+// settimeout
+// setinterval
+// Fetch API
+// Axios (or other HTTP libraries)
+// promise
