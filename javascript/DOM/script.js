ed51a3c50854a402b9bfc1316940cc49848a4be7
prmayanksingh <prmayanksingh05@gmail.com>
2024-09-05 16:58:23 +0530

    basics of dom

---
+++ javascript/DOM/script.js
@@ -0,0 +1,56 @@
+// selection
+// document.getElementById
+// document.getElementsByClassName
+// document.getElementsByTagName
+
+// save and select
+// var abcd = document.querySelector();
+
+// id
+// var abcd = document.querySelector('#abcd')
+
+// class
+// var classabcd = document.querySelector('.abcd')
+
+// tag
+// var h3 = document.querySelector("h3")
+
+// var firimg = document.querySelector("#abcd")
+// var secimg = document.querySelector(".abcd")
+
+// var firh3 = document.querySelector(".defg")
+// var sech3 = document.querySelector("#defg")
+
+// agar aapke paas baht saare h3 hai and aap select karte ho to srf pahla h3 select hoga
+// ager sare h3 select karna hai toh likhna padega
+// var h3 = document.querySelectorAll("h3")
+
+
+
+
+
+
+
+// Modifying Elements
+
+// kisi ko bhi change karne se pahle, pahla step hota hai usko select karna
+
+// textContent and innerHTML
+// var btn = document.querySelector("button");
+// btn.textContent = "Starting in 3 Seconds"
+
+// var h1 = document.querySelector("h1")
+
+// // h1.textContent = "i am gonna change this thing"
+// h1.textContent += "Mayank"
+
+// var h1 = document.querySelector("h1")
+// h1.innerHTML = '<i>hello</i>'
+
+
+
+
+
+
+
+
