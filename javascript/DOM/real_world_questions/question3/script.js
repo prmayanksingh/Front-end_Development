f6ab2676b027e02e8657743a0a81b54ff33a7ab3
prmayanksingh <prmayanksingh05@gmail.com>
2024-09-10 20:14:13 +0530

    question 3

--- javascript/DOM/real_world_questions/question3/script.js
+++ javascript/DOM/real_world_questions/question3/script.js
@@ -3,14 +3,40 @@
 // form ko submit karne par reload se rokne ke liye
 
 var form = document.querySelector("form");
-var inp1 = document.querySelector("inp1");
-var inp2 = document.querySelector("imp2");
+// var inp1 = document.querySelector("#inp1");
+// var inp2 = document.querySelector("#inp2");
+var inps = document.querySelectorAll('input[type="text"]')
+var h4 = document.querySelector("h4");
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    console.log(inp1.value);
-})
+    // if(inp1.value === '' || inp2.value ===  ''){
+    //     h4.textContent = "error, some fields are blank";
+    //     h4.style.color = "red";
+    // }
+    // else{
+    //     h4.textContent = "";
+    //     h4.style.color = "red";
+    // }
 
+    // inps.forEach(function(inp){
+    //     if(inp.value === ''){
+    //         h4.textContent = 'Error, some feilds are blank'
+    //         h4.style.color = 'red'
+    //     }
+    //     else{
+    //         h4.textContent = ''
+    //         h4.style.color = 'black'
+    //     }
+    // })
+
+    for(var i = 0; i<inps.length; i++){
+        if(inps[i].value.trim() === ''){
+            h4.textContent = 'Error, some feilds are blank';
+            h4.style.color = 'red';
+        }
+    }
+})
 
 
 
