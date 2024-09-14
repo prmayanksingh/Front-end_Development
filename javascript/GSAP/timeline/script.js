9ae38b103b378ddac5f7fa66dc8d0a3265191b3e
prmayanksingh <prmayanksingh05@gmail.com>
2024-09-14 23:24:33 +0530

    timeline basic

---
+++ javascript/GSAP/timeline/script.js
@@ -0,0 +1,32 @@
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+    x:1400,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    delay:1
+})
+
+tl.to("#box2",{
+    x:1400,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
+
+tl.to("#box3",{
+    x:1400,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+})
+
+
+
+
+
+
+
+
+
